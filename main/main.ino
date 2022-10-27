702aa750de6e42c842d10c1b59325a5d220f02ce
lucastu <86346920+lucastu@users.noreply.github.com>
2022-10-27 13:33:08 +0200

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -24,6 +24,14 @@ bool PM ;
 //Objects
 DS3231 Clock;
 
+byte lastMinute = 0;
+//TOTD = Time Of The Day
+byte startHeatingTOTD = 0;
+bool heatingProgrammed = false;
+bool relayState = false;
+byte minuteOftheDay;
+byte heatingDuration = 30; //in minute
+
 void setup()
 {
   pinMode(relayPin, OUTPUT);
@@ -33,9 +41,7 @@ void setup()
   //For the RTC
   Serial.begin(9600);
   Wire.begin(); //PIN XX & XX
-  //setDate(20, 30);
-	
-  drawCornerTest() //test from https://github.com/ZinggJM/GxEPD2/blob/master/examples/GxEPD2_Example/GxEPD2_Example.ino
+  //setDate(20, 30); 
 }
 
 void loop()
@@ -43,18 +49,50 @@ void loop()
   if (display_asked){
 	  display.fillScreen(GxEPD_WHITE);
 	  testdisplayValues();
+	  display_asked = false;
+	  drawCornerTest(); //test from https://github.com/ZinggJM/GxEPD2/blob/master/examples/GxEPD2_Example/GxEPD2_Example.ino
+  }
+	
+  //Update time on the screen if needed
+  if (Clock.getMinute() != lastMinute) updateTimeDisplay();
+  
+  //get the time of the day
+  TimeOftheDay = ReadTimeOfTheDay();
+	
+  if (heatingProgrammed){
+	  //Add displaying of the info
+	  if ((TimeOftheDay >= startHeatingTOTD) && (TimeOftheDay <= (startHeatingTOTD + heatingDuration))){
+		  Serial.println("heating");
+		  setFilPiloteState(true);
+	  }
+	  else	{
+		 Serial.println("stop heating");
+		 setFilPiloteState(false);
+		 heatingProgrammed = false;
+	 }
   }
   delay(1000);
 }
 
 void setDate(byte Hour, byte Minute) {
-	////Set Real Time Clock
+	//Set Real Time Clock
 	Clock.setClockMode(false); 	// set to 24h
 	Clock.setMinute(Minute);
 	Clock.setHour(Hour);
 	Clock.setSecond(0);
 	Serial.println("Clock set OK ! ");
 }
+
+byte TominuteOfTheDay(byte Hour, byte Minute) { //Transform the time in hour, minute to a count of all the minutes since the day started
+     byte minuteOftheDay= Hour*60 + Minute;
+     return minuteOftheDay;
+}
+
+byte ReadTimeOfTheDay(){
+     byte minuteOftheDay= Clock.getHour(h12, PM)*60 + Clock.getMinute();
+     return minuteOftheDay;
+}
+
 
 void setFilPiloteState(bool State){
 	// Commande le relais du fil pilote 
@@ -64,6 +102,36 @@ void setFilPiloteState(bool State){
 	else{
 		digitalWrite(relayPin, LOW);
 	}
+}
+
+void updateTimeDisplay(){
+  display.setRotation(1); // 0--> No rotation ,  1--> rotate 90 deg
+  uint16_t bg = GxEPD_WHITE;
+  uint16_t fg = GxEPD_BLACK;
+  u8g2Fonts.setFontMode(1);                 // use u8g2 transparent mode (this is default)
+  u8g2Fonts.setFontDirection(0);            // left to right (this is default)
+  u8g2Fonts.setForegroundColor(fg);         // apply Adafruit GFX color
+  u8g2Fonts.setBackgroundColor(bg);         // apply Adafruit GFX color
+  u8g2Fonts.setFont(u8g2_font_logisoso32_tr); //u8g2_font_logisoso32_tn--->numbers only to save memory ; u8g2_font_logisoso32_tr , u8g2_font_logisoso32_tf -->numbers&letters
+  
+  display.setPartialWindow(0, 0, display.width(), display.height()); //this sets a window for the partial update, so the values can update without refreshing the entire screen.
+  //https://forum.arduino.cc/t/e-paper-partial-update-i-dont-know-how/527555/8
+  //display.setPartialWindow(0, 90, 128, 25); // x,y,width,height
+  display.firstPage();
+  do
+  {
+    display.fillScreen(bg);
+    // Display first line
+    u8g2Fonts.setCursor(0, 0); 
+    Hour = Clock.getHour(h12, PM);
+    Hour = 2
+    Minute = Clock.getMinute();
+	  
+    u8g2Fonts.printf("%02d", Hour);
+    u8g2Fonts.print(":");
+    u8g2Fonts.printf(Minute, DEC);
+  }
+  while (display.nextPage());
 }
 
 void testdisplayValues()
@@ -91,23 +159,23 @@ void testdisplayValues()
   do
   {
     display.fillScreen(bg);
-
     // Display first line
-    u8g2Fonts.setCursor(x, y); 
-    Hour = Clock.getHour(h12, PM);
-    Hour = 2
-    Minute = Clock.getMinute();
+    //u8g2Fonts.setCursor(x, y); 
+    //Hour = Clock.getHour(h12, PM);
+    //Hour = 2
+    //Minute = Clock.getMinute();
 	  
-    u8g2Fonts.printf("%02d", Hour);
-    u8g2Fonts.print(":");
-    u8g2Fonts.printf(Minute, DEC);        
+    //u8g2Fonts.printf("%02d", Hour);
+    //u8g2Fonts.print(":");
+    //u8g2Fonts.printf(Minute, DEC);        
     // Display first line
     u8g2Fonts.setCursor(x-50, y+50); 
     u8g2Fonts.print("Mon Chaton");
   }
   while (display.nextPage());
 }
-void drawCornerTest()
+
+void drawCornerTest() //test fonction to be deleted
 {
   display.setFullWindow();
   display.setFont(u8g2_font_logisoso32_tr);
