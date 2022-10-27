2caa71c92df6215b60d52a3414875f1f9fb1ae25
lucastu <86346920+lucastu@users.noreply.github.com>
2022-10-27 10:43:21 +0200

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,26 +1,24 @@
-
+#define ENABLE_GxEPD2_GFX 0
 #define relayPin 5
 
-#define ENABLE_GxEPD2_GFX 0
 #include <GxEPD2_BW.h>
 #include <U8g2_for_Adafruit_GFX.h>
-#include <Wire.h>//https://www.arduino.cc/en/reference/wire
-#include <DS3231.h>//https://github.com/NorthernWidget/DS3231
+#include <Wire.h>
+#include <DS3231.h>
 
+//Definition of the screen (from the exemple of GxEPD2)
 #if defined(__AVR)
 #define MAX_DISPLAY_BUFFER_SIZE 800 
 #define MAX_HEIGHT(EPD) (EPD::HEIGHT <= MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8) ? EPD::HEIGHT : MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8))
 GxEPD2_BW<GxEPD2_213_T5D, MAX_HEIGHT(GxEPD2_213_T5D)> display(GxEPD2_213_T5D(/*CS=*/ SS, /*DC=*/ 8, /*RST=*/ 9, /*BUSY=*/ 7)); // GDEW0213T5D 104x212, UC8151D
-
 #endif
 
 U8G2_FOR_ADAFRUIT_GFX u8g2Fonts;
 
-bool display_asked = true;
 //Variables
-int Hour ;
-int Minute ;
-int Second ;
+bool display_asked = true;
+char Hour[] ;
+char Minute[] ;
 bool h12 ;
 bool PM ;
 //Objects
@@ -31,10 +29,13 @@ void setup()
   pinMode(relayPin, OUTPUT);
   display.init();
   u8g2Fonts.begin(display); // connect u8g2 procedures to Adafruit GFX
-  delay(1000);
+
+  //For the RTC
   Serial.begin(9600);
- 	Wire.begin();
+  Wire.begin(); //PIN XX & XX
   //setDate(20, 30);
+	
+  drawCornerTest() //test from https://github.com/ZinggJM/GxEPD2/blob/master/examples/GxEPD2_Example/GxEPD2_Example.ino
 }
 
 void loop()
@@ -43,27 +44,16 @@ void loop()
 	  display.fillScreen(GxEPD_WHITE);
 	  testdisplayValues();
   }
-  delay(1000);  
-  readRTC();
+  delay(1000);
 }
 
-void setDate(int Hour, int Minute) {
- 	////Set Real Time Clock
-
- 			Clock.setClockMode(false); 	// set to 24h
- 			Clock.setMinute(Minute);
- 			Clock.setHour(Hour);
-      Clock.setSecond(0);
-      Serial.println("Clock set OK ! ");
- 	
-}
-void readRTC( ) { /* function readRTC */
- 	////Read Real Time Clock
- 	Serial.print(Clock.getHour(h12, PM), DEC); //24-hr
- 	Serial.print(":");
- 	Serial.print(Clock.getMinute(), DEC);
- 	Serial.print(":");
- 	Serial.println(Clock.getSecond(), DEC);
+void setDate(byte Hour, byte Minute) {
+	////Set Real Time Clock
+	Clock.setClockMode(false); 	// set to 24h
+	Clock.setMinute(Minute);
+	Clock.setHour(Hour);
+	Clock.setSecond(0);
+	Serial.println("Clock set OK ! ");
 }
 
 void setFilPiloteState(bool State){
@@ -72,7 +62,7 @@ void setFilPiloteState(bool State){
 		digitalWrite(relayPin, HIGH);
 	}
 	else{
-	digitalWrite(relayPin, LOW);
+		digitalWrite(relayPin, LOW);
 	}
 }
 
@@ -85,13 +75,18 @@ void testdisplayValues()
   u8g2Fonts.setFontDirection(0);            // left to right (this is default)
   u8g2Fonts.setForegroundColor(fg);         // apply Adafruit GFX color
   u8g2Fonts.setBackgroundColor(bg);         // apply Adafruit GFX color
-  //u8g2Fonts.setFont(u8g2_font_helvR14_tf);  // select u8g2 font from here:  https://github.com/olikraus/u8g2/wiki/fntlistall
-
   u8g2Fonts.setFont(u8g2_font_logisoso32_tr); //u8g2_font_logisoso32_tn--->numbers only to save memory ; u8g2_font_logisoso32_tr , u8g2_font_logisoso32_tf -->numbers&letters
   
   uint16_t x = 50;
   uint16_t y = 50;
+	
   display.setPartialWindow(0, 0, display.width(), 296); //this sets a window for the partial update, so the values can update without refreshing the entire screen.
+  Serial.print("width:");
+  Serial.println(display.width());
+  Serial.print("height:");
+  Serial.println(display.height());
+  //https://forum.arduino.cc/t/e-paper-partial-update-i-dont-know-how/527555/8
+  //display.setPartialWindow(0, 90, 128, 25); // x,y,width,height
   display.firstPage();
   do
   {
@@ -99,15 +94,40 @@ void testdisplayValues()
 
     // Display first line
     u8g2Fonts.setCursor(x, y); 
-    u8g2Fonts.print(Clock.getHour(h12, PM), DEC);
+    Hour = Clock.getHour(h12, PM);
+    Hour = 2
+    Minute = Clock.getMinute();
+	  
+    u8g2Fonts.printf("%02d", Hour);
     u8g2Fonts.print(":");
-    u8g2Fonts.print(Clock.getMinute(), DEC);        
+    u8g2Fonts.printf(Minute, DEC);        
     // Display first line
     u8g2Fonts.setCursor(x-50, y+50); 
     u8g2Fonts.print("Mon Chaton");
-
-
-
   }
   while (display.nextPage());
+}
+void drawCornerTest()
+{
+  display.setFullWindow();
+  display.setFont(u8g2_font_logisoso32_tr);
+  display.setTextColor(GxEPD_BLACK);
+  for (uint16_t r = 0; r <= 4; r++)
+  {
+    display.setRotation(r);
+    display.firstPage();
+    do
+    {
+      display.fillScreen(GxEPD_WHITE);
+      display.fillRect(0                   , 0                     , 8 , 8 , GxEPD_BLACK);
+      display.fillRect(display.width() - 18, 0                     , 16, 16, GxEPD_BLACK);
+      display.fillRect(display.width() - 25, display.height() - 25 , 24, 24, GxEPD_BLACK);
+      display.fillRect(0                   , display.height() - 33 , 32, 32, GxEPD_BLACK);
+      
+      display.setCursor(display.width() / 2, display.height() / 2);
+      display.print(display.getRotation());
+    }
+    while (display.nextPage());
+    delay(1000);
+  }
 }
